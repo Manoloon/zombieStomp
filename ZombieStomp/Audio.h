c5376b463de952d6e83a90006bf1b821d45ce916
mordor\manoloon <pablitensehen@gmail.com>
2018-02-25 19:13:49 -0300

    audio check _ preparing before oop refactoring

---
+++ ZombieStomp/Audio.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "SFML\Audio.hpp"
+
+class Audio 
+{
+private: 
+	sf::SoundBuffer HitBuffer;
+	sf::SoundBuffer SplatterBuffer;
+	sf::SoundBuffer ShootBuffer;
+	sf::SoundBuffer ReloadBuffer;
+	sf::SoundBuffer ReloadFailBuffer;
+	sf::SoundBuffer PowerUpBuffer;
+	sf::SoundBuffer PickupBuffer;
+	void SetAudio(sf::SoundBuffer soundBuffer,)
+
+public :
+	
+
+};
